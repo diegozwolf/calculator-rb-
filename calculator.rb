9c6810013928a02692f803fd16d6d534035b9e5a
Diego Zuluaga <diegozuluaga88@gmail.com>
2018-03-15 11:38:46 -0500

    test pass

--- calculator.rb
+++ calculator.rb
@@ -1,27 +1,58 @@
 class Calculator
-
-  def initialize
-    @val1 = 0
-    @val2 = 0
-  end
   def self.add(val1, val2)
-    @val1 = val1
-    @val2 = val2
-    @val1 + @val2
+    val1 + val2
   end
   def self.substract(val1, val2)
-    @val1 = val1
-    @val2 = val2
-    @val1 - @val2
+    val1 - val2
   end
   def self.multi(val1, val2)
-    @val1 = val1
-    @val2 = val2
-    @val1 * @val2
+    val1 * val2
   end
   def self.divide(val1, val2)
-    @val1 = val1
-    @val2 = val2
-    @val1 / @val2
+    val1 / val2
   end
 end
+
+# class Calculator
+#   def Calculator.add(val1, val2)
+#     val1 + val2
+#   end
+#
+#   def Calculator.substract(val1, val2)
+#     val1 - val2
+#   end
+#   def Calculator.multi(val1, val2)
+#     val1 * val2
+#   end
+#   def Calculator.divide(val1, val2)
+#     val1 / val2
+#   end
+# end
+
+# class Calculator
+#   def self.select_operation(val1, val2, ope)
+#     @val1 = val1
+#     @val2 = val2
+#     values
+#     return self.add if ope == 'add'
+#     return self.add_splat if ope == 'add_splat'
+#     return self.subs if ope == 'subs'
+#     return self.multi if ope == 'multi'
+#     return self.divide if ope == 'divide'
+#   end
+#   def self.add
+#     @val1 + @val2
+#   end
+#   def self.add_splat(*args)
+#     args.sum
+#   end
+#   def self.subs
+#     @val1 - @val2
+#   end
+#   def self.multi
+#     @val1 * @val2
+#   end
+#   def self.divide
+#     @val1 / @val2
+#   end
+# end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -12,7 +12,7 @@ describe Calculator do
     end
   end
   describe '::multi' do
-    it 'should return substracion of two integers' do
+    it 'should return multiplication of two integers' do
       expect(Calculator.multi(5, 3)).to eq 15
     end
   end
@@ -24,3 +24,32 @@ describe Calculator do
   end
 
 end
+
+# describe Calculator do
+#   describe '::add' do
+#     it 'should return sum of two integers' do
+#       expect(Calculator.select_operation(7, 7, 'add')).to eq 14
+#     end
+#
+#     describe '::add' do
+#       it 'should return sum of 4 integers' do
+#         expect(Calculator.add_splat(7, 2, 3, 4)).to eq 16
+#       end
+#     end
+#   end
+#   describe '::subs' do
+#     it 'should return substracion of two integers' do
+#       expect(Calculator.select_operation(6, 5, 'subs')).to eq 1
+#     end
+#   end
+#   describe '::multi' do
+#     it 'should return multiplicatio of two integers' do
+#       expect(Calculator.select_operation(8, 5, 'multi')).to eq 40
+#     end
+#   end
+#   describe '::divide' do
+#     it 'should return sum of two integers' do
+#       expect(Calculator.select_operation(6, 3, 'divide')).to eq 2
+#     end
+#   end
+# end
