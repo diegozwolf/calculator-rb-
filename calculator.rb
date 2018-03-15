4baa953a55ec44d6384b21947a00f095eb35b567
Diego Zuluaga <diegozuluaga88@gmail.com>
2018-03-15 10:05:28 -0500

    add basic operations before example class

--- calculator.rb
+++ calculator.rb
@@ -9,4 +9,19 @@ class Calculator
     @val2 = val2
     @val1 + @val2
   end
+  def self.substract(val1, val2)
+    @val1 = val1
+    @val2 = val2
+    @val1 - @val2
+  end
+  def self.multi(val1, val2)
+    @val1 = val1
+    @val2 = val2
+    @val1 * @val2
+  end
+  def self.divide(val1, val2)
+    @val1 = val1
+    @val2 = val2
+    @val1 / @val2
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,4 +6,21 @@ describe Calculator do
       expect(Calculator.add(7, 7)).to eq 14
     end
   end
+  describe '::substract' do
+    it 'should return substracion of two integers' do
+      expect(Calculator.substract(50, 30)).to eq 20
+    end
+  end
+  describe '::multi' do
+    it 'should return substracion of two integers' do
+      expect(Calculator.multi(5, 3)).to eq 15
+    end
+  end
+
+  describe '::divide' do
+    it 'should return substracion of two integers' do
+      expect(Calculator.divide(20, 4)).to eq 5
+    end
+  end
+
 end
